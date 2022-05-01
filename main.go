9aad136f650d8d4d294580f9c42136925e20e96a
QQQ1688 <100270715+QQQ1688@users.noreply.github.com>
2022-05-01 18:17:18 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/mysql", controller.GetDatas)
 	router.GET("/mysql/:ip", controller.GetDataByIP)
-	// router.POST("/albums", postAlbums)
+
 	// Use the Run function to attach the router to an http.Server and start the server.
 	router.Run("localhost:8080")
 }
